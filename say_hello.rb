f8bae4f9e85b32e5996c2589cfd6dc5b32fd34e1
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-13 19:49:19 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello(name="Hello Ruby Programmer!")
+def say_hello(name)
   puts "Hello #{name} !"
 end
 
-say_hello("Gabriella")
+say_hello("Kent Beck")
